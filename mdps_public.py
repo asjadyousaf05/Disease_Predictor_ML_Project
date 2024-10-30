4ac9cff945f39c69e89ccc028f249bd69f7e3542
asjadyousaf05 <asjadyousafkhan07@gmail.com>
2024-10-31 01:04:25 +0500

    New Add

--- mdps_public.py
+++ mdps_public.py
@@ -12,11 +12,11 @@ from streamlit_option_menu import option_menu
 
 # loading the saved models
 
-diabetes_model = pickle.load(open('diabetes_model.sav', 'rb'))
+diabetes_model = pickle.load(open(r'C:\Users\asjad\OneDrive\Desktop\bio projecy\drive-download-20241029T175550Z-001\diabetes_model.sav', 'rb'))
 
-heart_disease_model = pickle.load(open('heart_disease_model.sav', 'rb'))
+heart_disease_model = pickle.load(open(r'C:\Users\asjad\OneDrive\Desktop\bio projecy\drive-download-20241029T175550Z-001\heart_disease_model.sav', 'rb'))
 
-parkinsons_model = pickle.load(open('parkinsons_model.sav', 'rb'))
+parkinsons_model = pickle.load(open(r'C:\Users\asjad\OneDrive\Desktop\bio projecy\drive-download-20241029T175550Z-001\parkinsons_model.sav', 'rb'))
 
 
 
